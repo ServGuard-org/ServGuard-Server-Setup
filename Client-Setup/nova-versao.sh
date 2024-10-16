b5886ed7bf9d880934664cbbb9f1ac650f1a2b1d
marcio <marcio.juliao@sptech.school>
2024-10-15 21:44:03 -0300

    Versão nova - funcionando

--- Client-Setup/nova-versao.sh
+++ Client-Setup/nova-versao.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-# Função para verificar se um comando está disponível
-checar_comando() {
-    command -v "$1" >/dev/null 2>&1 # command retorna 0 se o comando for encontrado e 1 se não for.
-}
-
-# Verifica e instala o Python
-if checar_comando python3; then
-    echo "Python já está instalado."
     sudo apt update
     sudo apt install -y python3 python3-pip
     cd "$HOME/ServGuard-Python/"
@@ -15,34 +7,13 @@ if checar_comando python3; then
     python3 -m venv venv
     source venv/bin/activate
     pip install -r requirements.txt
-else
-    echo "Python não encontrado. Instalando..."
-    sudo apt update
-    sudo apt install -y python3 python3-pip
-    echo "Python instalado."
-fi
-
-# Verifica e instala o Java 17
-if checar_comando java; then
-    echo "Java já está instalado."
-else
-    echo "Java não encontrado. Instalando Java 17..."
     sudo apt update
     sudo apt install -y openjdk-17-jdk
     echo "Java 17 instalado."
-fi
-
-# Verifica e instala o Git
-if checar_comando git; then
-    echo "Git já está instalado."
-else
-    echo "Git não encontrado. Instalando..."
     sudo apt update
     sudo apt install -y git
     echo "Git instalado."
-fi
 
-# Diretório do repositório de Python Git
 REPO_DIR_PY="$HOME/ServGuard-Python"
 
 # Verifica se o diretório do repositório PY existe
@@ -97,3 +68,7 @@ python3 -m venv venv
 source venv/bin/activate
 pip install -r requirements.txt
 python3 setup-inicial.py
+
+# venv/bin/activate
+# USAR source venv/bin/activate PARA ATIVAR O VENV PYTHON PARA RODAR O SCRIPT 
+# SEMPRE QUE FOR EXECUTAR O SCRIPT PYTHON NO TERMINAL DO LINUX (BASH) 
